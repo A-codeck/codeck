ccefdb727603ef5bb51255e64e2d1fd8d8bd4fe5
Octavio Carneiro <ocarneiro1@gmail.com>
2025-06-10 10:44:15 -0300

    feat: add working db on docker

--- backend/main.go
+++ backend/main.go
@@ -26,9 +26,6 @@ func main() {
 	routes.RegisterGroupRoutes(r, groupController)
 	routes.RegisterActivityRoutes(r, activityController)
 
-	log.Println("Joao is running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
-
 	log.Println("Trying to migrate")
 	dsn := "host=db user=my_usr password=my_pwd dbname=codeck port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -38,4 +35,8 @@ func main() {
 	if err := db.AutoMigrate(&group.Group{}, &activity.Activity{}); err != nil {
 		log.Fatalf("Failed to migrate database: %v", err)
 	}
+
+	log.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
